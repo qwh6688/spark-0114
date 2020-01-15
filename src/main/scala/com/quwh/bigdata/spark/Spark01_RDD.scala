adbad671026cc489a5df9a933c88dcf98ebea372
quwh <1422682170@qq.com>
2020-01-15 17:37:19 +0800

    11

--- src/main/scala/com/quwh/bigdata/spark/Spark01_RDD.scala
+++ src/main/scala/com/quwh/bigdata/spark/Spark01_RDD.scala
@@ -1,7 +1,22 @@
 package com.quwh.bigdata.spark
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
 object Spark01_RDD {
   def main(args: Array[String]): Unit = {
+    val config = new SparkConf().setMaster("local[*]").setAppName("WordCount")
+    val sc = new SparkContext(config)
+    //1) 内存中创建makeRDD
+    val listRDD: RDD[Int] = sc.makeRDD(List(1,2,3))
+    //parallelize
+    val arrayRDD: RDD[String] = sc.parallelize(Array("1","2","3","曲文辉"))
+//    listRDD.collect().foreach(println)
+    arrayRDD.collect().foreach(println)
+
+    //2) 外部存储中创建
+    val fileRDD: RDD[String] = sc.textFile("input/")
+
 
   }
 
--- src/main/scala/com/quwh/bigdata/spark/WordCount.scala
+++ src/main/scala/com/quwh/bigdata/spark/WordCount.scala
@@ -7,7 +7,7 @@ object WordCount {
   def main(args: Array[String]): Unit = {
     val config: SparkConf = new SparkConf().setMaster("local[*]").setAppName("WordCount")
     val sc: SparkContext = new SparkContext(config)
-    val lines: RDD[String] = sc.textFile("D:\\idea workspace\\spark-0114\\input\\word.txt")
+    val lines: RDD[String] = sc.textFile("D:\\develop\\ideaWorkspace\\spark-0114\\input\\word.txt")
     val words: RDD[String] = lines.flatMap(_.split(" "))
     val wordToOne: RDD[(String, Int)] = words.map((_,1))
     val wordToSum: RDD[(String, Int)] = wordToOne.reduceByKey(_+_)
