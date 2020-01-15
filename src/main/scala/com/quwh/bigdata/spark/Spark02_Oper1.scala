ace1c450ddc8b60eac4393a7990290bf29aa478f
quwh <1422682170@qq.com>
2020-01-15 17:38:26 +0800

    22

---
+++ src/main/scala/com/quwh/bigdata/spark/Spark02_Oper1.scala
@@ -0,0 +1,20 @@
+package com.quwh.bigdata.spark
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+/*
+map的使用
+ */
+object Spark02_Oper1 {
+  def main(args: Array[String]): Unit = {
+    val config = new SparkConf().setMaster("local[*]").setAppName("WordCount")
+    val sc = new SparkContext(config)
+    val listRDD: RDD[Int] = sc.makeRDD(1 to 10)
+    //val mapRDD: RDD[Int] = listRDD.map(x=>x*2)
+    val mapRDD: RDD[Int] = listRDD.map(_*2)
+    mapRDD.collect().foreach(println)
+
+
+  }
+
+}
---
+++ src/main/scala/com/quwh/bigdata/spark/Spark02_Oper2.scala
@@ -0,0 +1,21 @@
+package com.quwh.bigdata.spark
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark01_Oper1 {
+  def main(args: Array[String]): Unit = {
+    val config = new SparkConf().setMaster("local[*]").setAppName("WordCount")
+    val sc = new SparkContext(config)
+    val listRDD: RDD[Int] = sc.makeRDD(1 to 10)
+   /* val mapPartitionsRDD: RDD[Int] = listRDD.mapPartitions(dataset => {
+      dataset.map(date => date * 2)
+    })*/
+//    val mapPartitionsRDD: RDD[Int] = listRDD.mapPartitions(_.map(_*2))
+    val mapPartitionsRDD: RDD[Int] = listRDD.mapPartitions(x=>x.map(_*2))
+    mapPartitionsRDD.collect().foreach(println)
+
+
+  }
+
+}
