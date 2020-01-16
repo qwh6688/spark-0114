5df48b549f6d679a1467093d8e0ca710e14489ab
quwh <1422682170@qq.com>
2020-01-16 10:28:07 +0800

    最大值和 X+“”

--- src/main/scala/com/quwh/bigdata/spark/Test.scala
+++ src/main/scala/com/quwh/bigdata/spark/Test.scala
@@ -8,6 +8,8 @@ object Test {
     val config = new SparkConf().setMaster("local[*]").setAppName("WordCount")
     val sc = new SparkContext(config)
     val listRDD: RDD[Int] = sc.makeRDD(1 to 10)
+    val maxValue: Int = listRDD.max()
+    println("最大值: "+maxValue)
     val mapRDD: RDD[String] = listRDD.map(x=>"X"+x)
     mapRDD.collect().foreach(println)
   }
