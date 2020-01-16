db24fa1a1b858b21d8df3cf3a17705e7a56fb746
quwh <1422682170@qq.com>
2020-01-16 10:26:27 +0800

    filter算子

---
+++ src/main/scala/com/quwh/bigdata/spark/Spark09_Oper8.scala
@@ -0,0 +1,27 @@
+package com.quwh.bigdata.spark
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/*
+groupBy
+作用：分组，按照传入函数的返回值进行分组。将相同的key对应的值放入一个迭代器。
+ */
+object Spark09_Oper8 {
+  /**
+    * filter
+    * 2. 需求：创建一个RDD，按照元素模以2的值进行分组。
+    *
+    * @param args
+    */
+  def main(args: Array[String]): Unit = {
+    val config = new SparkConf().setMaster("local[*]").setAppName("WordCount")
+    val sc = new SparkContext(config)
+    val listRDD: RDD[Int] = sc.makeRDD(List(1, 2, 3, 4), 3)
+    val filterRDD: RDD[Int] = listRDD.filter(i=>i%2==0)
+    filterRDD.collect().foreach(println)
+
+
+
+  }
+}
