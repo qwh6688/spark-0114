ea2b65c0569e1c80a1881ae8f33cffce8d208457
quwh <1422682170@qq.com>
2020-01-16 11:31:23 +0800

    coalesce算子

--- src/main/scala/com/quwh/bigdata/spark/Spark11_Oper10.scala
+++ src/main/scala/com/quwh/bigdata/spark/Spark11_Oper10.scala
@@ -7,18 +7,18 @@ import org.apache.spark.{SparkConf, SparkContext}
  */
 object Spark11_Oper10 {
   /**
+    * coalesce算子
     *
     * @param args
     */
   def main(args: Array[String]): Unit = {
     val config = new SparkConf().setMaster("local[*]").setAppName("WordCount")
     val sc = new SparkContext(config)
-    val listRDD: RDD[Int] = sc.makeRDD(List(1,2,1,5,2,9,6,1))
-    val distinctRDD: RDD[Int] = listRDD.distinct(2)
-//    distinctRDD.collect().foreach(println)
-    //打印到两个分区里
-    distinctRDD.saveAsTextFile("output")
-
+    val listRDD: RDD[Int] = sc.makeRDD(1 to 16, 4)
+    println("缩减前的分区： " + listRDD.partitions.size)
+    val coalesceRDD: RDD[Int] = listRDD.coalesce(3)
+    println("缩减后的分区： " + coalesceRDD.partitions.size)
+    coalesceRDD.saveAsTextFile("output")
 
 
   }
