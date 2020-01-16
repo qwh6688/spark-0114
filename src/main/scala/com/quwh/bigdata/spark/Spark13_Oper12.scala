c38b43661ad10060c600c9ff4ae614439e01e517
quwh <1422682170@qq.com>
2020-01-16 17:47:57 +0800

    k-V类型算子 partitionBy 自定义分区器

--- src/main/scala/com/quwh/bigdata/spark/Spark13_Oper12.scala
+++ src/main/scala/com/quwh/bigdata/spark/Spark13_Oper12.scala
@@ -7,6 +7,7 @@ import org.apache.spark.{Partitioner, SparkConf, SparkContext}
  */
 object Spark13_Oper12 {
   /**
+    * 数据类型必须是k-V类型，不然此算子不能用。
     * key-Value 类型的算子
     * partitionBy 重新分区
     * 自定义分区器
@@ -19,6 +20,7 @@ object Spark13_Oper12 {
     val listRDD: RDD[(String, Int)] = sc.makeRDD(List(("a", 1), ("b", 2), ("c", 3)))
     //分区规则
     val partByRDD: RDD[(String, Int)] = listRDD.partitionBy(new MyPartitioner(3))
+
     partByRDD.saveAsTextFile("output")
     sc.stop()
 
