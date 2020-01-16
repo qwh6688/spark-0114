bc0fd109206b9d7c2f22474dbb0c35db2de6e79f
quwh <1422682170@qq.com>
2020-01-16 10:37:11 +0800

    coalesce算子

---
+++ src/main/scala/com/quwh/bigdata/spark/Spark11_Oper10.scala
@@ -0,0 +1,25 @@
+package com.quwh.bigdata.spark
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/*
+ */
+object Spark11_Oper10 {
+  /**
+    *
+    * @param args
+    */
+  def main(args: Array[String]): Unit = {
+    val config = new SparkConf().setMaster("local[*]").setAppName("WordCount")
+    val sc = new SparkContext(config)
+    val listRDD: RDD[Int] = sc.makeRDD(List(1,2,1,5,2,9,6,1))
+    val distinctRDD: RDD[Int] = listRDD.distinct(2)
+//    distinctRDD.collect().foreach(println)
+    //打印到两个分区里
+    distinctRDD.saveAsTextFile("output")
+
+
+
+  }
+}
