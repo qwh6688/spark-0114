2644f9ae6552763d883dfe62d916856f326fabbd
quwh <1422682170@qq.com>
2020-01-16 10:10:08 +0800

    数组操作 mkString，元素之间用 ，号隔开

---
+++ src/main/scala/com/quwh/bigdata/spark/Spark06_Oper501.scala
@@ -0,0 +1,33 @@
+package com.quwh.bigdata.spark
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/*
+将每一个分区形成一个数组，形成新的RDD类型时RDD[Array[T]]
+求每个分区里面的最大值
+ */
+object Spark06_Oper501 {
+  // glom
+  def main(args: Array[String]): Unit = {
+    val config = new SparkConf().setMaster("local[*]").setAppName("WordCount")
+    val sc = new SparkContext(config)
+    val listRDD: RDD[Int] = sc.makeRDD(1 to 8, 3)
+    val glomRDD: RDD[Array[Int]] = listRDD.glom()
+    //    glomRDD.collect().foreach(println)
+    /*  glomRDD.collect().foreach(
+        array => {
+          array.foreach(println)
+        }
+      )*/
+    /*    glomRDD.collect().foreach(array => {
+          for (elem <- array) {
+            println(elem)
+          }
+        })
+
+      */
+    //array.mkString 打印数组,中间用, 号隔开
+    glomRDD.collect().foreach(array => println(array.mkString(",").max))
+  }
+}
